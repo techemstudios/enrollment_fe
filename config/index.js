00bd53db957d8f36e24699bcb8c176d8edd85769
wray <wray@wrayesian.com>
2018-08-09 17:10:23 -0400

    assets path

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: 'enrollment_fe',
 
     /**
      * Source Maps
