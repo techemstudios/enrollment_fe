13b8ce28f9587c79710a4f2d7f9999daf1c6dc39
wray <wray@wrayesian.com>
2018-08-09 21:52:45 -0400

    you've been spooked

--- src/App.vue
+++ src/App.vue
@@ -11,13 +11,12 @@
           <img src="../src/assets/loader.gif"/>
           Loading...
         </div>
-
       <div class="wrapper">
         <div class="row">
           <div v-for="section in sections" :key="section.id">
-          <div class="col-md-4 cards">
-             <img src="../src/assets/spaghett.jpg" class="img-responsive" alt="Random images placeholder">
-            <div>
+          <div class="col-md-4 cards" v-on:click="getSection(section)">
+           <img v-show="selected_section.id==section.id" src="../src/assets/spaghett.jpg" class="img-responsive" alt="Random images placeholder">
+           <div>
               <h3>{{ section.id }}</h3>
               <p>{{ section.description }}</p>
               <p>{{ section.start_time }}</p>
@@ -38,7 +37,8 @@ export default {
   data () {
     return {
       sections: [],
-      loading: false
+      loading: false,
+      selected_section: false
     }
   },
   methods: {
@@ -48,6 +48,19 @@ export default {
         .then((response) => {
           this.loading = false
           this.sections = response.data
+        }, (err) => {
+          if (err) {
+            console.log(err.stack)
+          }
+          this.loading = false
+        })
+    },
+    getSection: function (section) {
+      this.loading = true
+      axios.get('https://j5ul824zvb.execute-api.us-east-1.amazonaws.com/dev/api/sections/' + section.id)
+        .then((response) => {
+          this.loading = false
+          this.selected_section = response.data
         }, (err) => {
           if (err) {
             console.log(err.stack)
@@ -102,7 +115,7 @@ button {
 }
 .cards {
   background: #F5F5F5;
-  height:400px;
+  height:100px;
 }
  .cards:hover {
   transform: translateY(-0.5em);
